91ac5e2c44024103213a15dd58809f378ffe4b9d
arulgoyal <arulgoyal6@gmail.com>
2023-11-24 13:02:50 +0530

    added fonts and cleared boiler plate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import clsx from 'clsx'
+import { Inter,Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  variable: '--font-plus-jakarta-sans',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={clsx(inter.className,plusJakartaSans.variable)}>
+      <body >{children}</body>
     </html>
   )
 }
